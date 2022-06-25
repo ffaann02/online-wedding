37e3ce48fc19ab1ba52b5dda54909969928ab04b
Faan02 <rudfanno1@gmail.com>
2022-06-25 23:15:08 +0700

    fix details all pages to responsive desktop-tablet-mobile

--- pages/wish.js
+++ pages/wish.js
@@ -17,7 +17,7 @@ const EditButton = styled(Button)({
 export default function Wish() {
     return (
         <Grid item lg={9} md={9} sm={11} xs={12} sx={{ height: { lg: "100%", md: "100%", sm: "100vh", xs: "100vh" } }}>
-            <Box sx={{ width: "100%", height: "100%", padding: { lg: "0 40px 0 140px", sm: "0" }, marginTop: "80px" }}>
+            <Box sx={{ width: "100%", height: "100%", padding: { lg: "0 40px 0 140px",md:"0 20px 0 40px", sm: "0px 20px",xs:"0 20px" }, marginTop: "80px" }}>
                 
                 <Box sx={{
                     display: 'flex', backgroundColor: "#FCD5CE"
@@ -26,25 +26,25 @@ export default function Wish() {
                     <Typography variant="h6" sx={{ fontSize: "16px" }}>ข้อมูลเกี่ยวกับการ์ดอวยพร</Typography>
                 </Box>
                 <Grid container sx={{ width: "100%", padding: "20px 10px", marginTop: "10px" }}>
-                    <Grid item lg={12} sx={{ display: "flex" ,margin:"10px 0"}}>
-                        <Grid item lg={3} md={12}>
+                    <Grid item lg={12} md={12} sm={12} xs={12} sx={{ display: "flex" ,margin:"10px 0"}}>
+                        <Grid item lg={3} md={3} sm={3} xs={4}>
                             <Typography variant="a" sx={{ fontSize: "16px", color: "#6D6D64" }}>สถานะ</Typography>
                         </Grid>
-                        <Grid item lg={7}>
+                        <Grid item lg={7} md={7} sm={7} xs={6}>
                             <Typography variant="a" sx={{ fontSize: "16px", fontWeight: "600" }}>เปิดรับ</Typography>
                         </Grid>
-                        <Grid item lg={2} sx={{ textAlign: "right" }}>
+                        <Grid item lg={2} md={2} sm={2} xs={2} sx={{ textAlign: "right" }}>
                             <EditButton>แก้ไข</EditButton>
                         </Grid>
                     </Grid>
-                    <Grid item lg={12} sx={{ display: "flex" ,margin:"10px 0"}}>
-                        <Grid item lg={3} md={12}>
+                    <Grid item lg={12} md={12} sm={12} xs={12} sx={{ display: "flex" ,margin:"10px 0"}}>
+                        <Grid item lg={3} md={3} sm={3} xs={4}>
                             <Typography variant="a" sx={{ fontSize: "16px", color: "#6D6D64" }}>จำนวน</Typography>
                         </Grid>
-                        <Grid item lg={7}>
+                        <Grid item lg={7} md={7} sm={7} xs={6}>
                             <Typography variant="a" sx={{ fontSize: "16px", fontWeight: "600" }}>xx ใบ</Typography>
                         </Grid>
-                        <Grid item lg={2} sx={{ textAlign: "right" }}>
+                        <Grid item lg={2} md={2} sm={2} xs={2} sx={{ textAlign: "right" }}>
                             <EditButton>ดู</EditButton>
                         </Grid>
                     </Grid>
